fede3a19f0432e148f44fdd33f0920d0056421b8
Tino Bokoshev <bokoshevtino@gmail.com>
2023-12-02 16:25:02 +0100

    created repo and first commit

---
+++ src/Dbtest.java
@@ -0,0 +1,11 @@
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Dbtest {
+    //db parameters
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/newdb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123";
+
+
+}
